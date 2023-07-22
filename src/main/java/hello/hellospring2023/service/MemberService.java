954888428c74640d30f195188d4847eb92e3d6ef
EunJung803 <aimee3322@gmail.com>
2023-07-22 23:35:50 +0900

    feat : MemberService added

---
+++ src/main/java/hello/hellospring2023/service/MemberService.java
@@ -0,0 +1,43 @@
+package hello.hellospring2023.service;
+
+import hello.hellospring2023.domain.Member;
+import hello.hellospring2023.repository.MemberRepository;
+import hello.hellospring2023.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름을 가진 중복 회원은 X
+        validateDuplicateMember(member);    // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())       // Optional로 한번 감싼 뒤
+                .ifPresent(m -> {                           // ifPresent로 존재하는지 확인해보기
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
